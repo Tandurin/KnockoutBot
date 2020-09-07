25c1c8365e62941bb0002665757ff6579b93ab6d
Tandurin <dennisvlaanderen@gmail.com>
2020-09-07 13:27:23 +0200

    Server announcements
    First draft for basic server announcements. And client-side messaging.

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from discord.ext.commands import Bot as BotBase
+from discord import Embed, File
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 PREFIX = "+"
@@ -29,10 +32,32 @@ class Bot(BotBase):
 		print("Bot Disconnected")
 
 	async def on_ready(self):
+
+		avatar = self.user.avatar_url
+		fileLocation = "./data/images/profile.png"
+
 		if not self.ready:
 			self.ready = True
 			self.guild = self.get_guild(435119526200213514)
 			print("bot ready")
+
+			channel = self.get_channel(752482031228682340)
+			await channel.send("Now online!")
+
+			embed = Embed(title="Now Online!", description="KnockoutBot is now online.", colour=0xFF00A4, timestamp=datetime.utcnow())
+			fields = [("Name", "Value", True),
+					  ("Another field", "This field is next to the other one.", True),
+					  ("A non-inline field", "This field will appear on it's own row.", False)]
+			for name, value, inline in fields:
+				embed.add_field(name=name, value=value, inline=inline)
+			embed.set_author(name="KnockoutBot", icon_url=avatar)
+			embed.set_footer(text="This is a footer!")
+			embed.set_thumbnail(url=avatar)
+			embed.set_image(url=avatar)
+			await channel.send(embed=embed)
+
+			await channel.send(file=File(fileLocation))
+
 
 		else:
 			print("bot reconnected")

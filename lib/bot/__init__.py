17febc34ee7b022e25458d58f31671d85a9c5295
Tandurin <dennisvlaanderen@gmail.com>
2020-09-12 14:39:58 +0200

    New gitignore to protect userData, new database handling & basic timers

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -7,6 +7,8 @@ from discord.ext.commands import CommandNotFound
 from discord import Embed, File
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+from ..db import db
+
 # Declare Global Vars
 PREFIX = "+"
 OWNER_IDS = [210003268082991104]
@@ -21,6 +23,8 @@ class Bot(BotBase):
         self.ready = False
         self.guild = None
         self.scheduler = AsyncIOScheduler()
+
+        db.autosave(self.scheduler)
 
 # Super __init__ as class copy
         super().__init__(command_prefix=PREFIX, owners_ids=OWNER_IDS)
@@ -67,37 +71,37 @@ class Bot(BotBase):
     async def on_ready(self):
 
         # Define local method variables
-        avatar = self.user.avatar_url
-        fileLocationAvatar = "./data/images/profile.png"
-        baseColour = 0xFF00A4
+        # avatar = self.user.avatar_url
+        # fileLocationAvatar = "./data/images/profile.png"
+        # baseColour = 0xFF00A4
 
         if not self.ready:
             # Set connected state
             self.ready = True
             self.guild = self.get_guild(435119526200213514)
-            print("Bot Ready For Operation")
+            self.scheduler.start()
 
             # Set Command-dump channel
             channel = self.get_channel(752482031228682340)
             await channel.send("Now online!")
 
-            embed = Embed(title="Now Online!",
-                          description="KnockoutBot is now online.",
-                          colour=baseColour, timestamp=datetime.utcnow())
-            fields = [("Name", "Value", True),
-                      ("Another field", "This field is next to the other one.",
-                       True),
-                      ("A non-inline field",
-                       "This field will appear on it's own row.", False)]
-            for name, value, inline in fields:
-                embed.add_field(name=name, value=value, inline=inline)
-            embed.set_author(name="KnockoutBot", icon_url=avatar)
-            embed.set_footer(text="This is a footer!")
-            embed.set_thumbnail(url=avatar)
-            embed.set_image(url=avatar)
-            await channel.send(embed=embed)
+#            embed = Embed(title="Now Online!",
+#                          description="KnockoutBot is now online.",
+#                          colour=baseColour, timestamp=datetime.utcnow())
+#            fields = [("Name", "Value", True),
+#                      ("Another field", "This field is next to the other one.",
+#                       True),
+#                      ("A non-inline field",
+#                       "This field will appear on it's own row.", False)]
+#            for name, value, inline in fields:
+#                embed.add_field(name=name, value=value, inline=inline)
+#            embed.set_author(name="KnockoutBot", icon_url=avatar)
+#            embed.set_footer(text="This is a footer!")
+#            await channel.send(embed=embed)
+#
+#            await channel.send(file=File(fileLocationAvatar))
 
-            await channel.send(file=File(fileLocationAvatar))
+            print("Bot Ready For Operation")
 
         else:
             print("bot reconnected")

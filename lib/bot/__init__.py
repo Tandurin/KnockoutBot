31e0f339f34824647ca2eea9e5d13b345b9b59aa
Tandurin <dennisvlaanderen@gmail.com>
2020-09-12 19:42:48 +0200

    Basic Cog system implementation + loading system

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,6 +1,8 @@
 # Declare Imports
 
+from asyncio import sleep
 from datetime import datetime
+from glob import glob
 
 from discord.ext.commands import Bot as BotBase
 from discord.ext.commands import CommandNotFound
@@ -12,6 +14,20 @@ from ..db import db
 # Declare Global Vars
 PREFIX = "+"
 OWNER_IDS = [210003268082991104]
+COGS = [path.split("\\")[-1][:-3] for path in glob("./lib/cogs/*.py")]
+
+
+class Ready(object):
+    def __init__(self):
+        for cog in COGS:
+            setattr(self, cog, False)
+
+    def ready_up(self, cog):
+        setattr(self, cog, True)
+        print(f"{cog} cog ready_up")
+
+    def all_ready(self):
+        return all([getattr(self, cog) for cog in COGS])
 
 
 # Define basic operation class
@@ -21,6 +37,8 @@ class Bot(BotBase):
         # Declare local method variables
         self.PREFIX = PREFIX
         self.ready = False
+        self.cogs_ready = Ready()
+
         self.guild = None
         self.scheduler = AsyncIOScheduler()
 
@@ -29,9 +47,19 @@ class Bot(BotBase):
 # Super __init__ as class copy
         super().__init__(command_prefix=PREFIX, owners_ids=OWNER_IDS)
 
+    def setup(self):
+        for cog in COGS:
+            self.load_extension(f"lib.cogs.{cog}")
+            print(f" {cog} cog loaded")
+
+        print("Setup complete")
+
 # Boot function
     def run(self, version):
         self.VERSION = version
+
+        print("Running setup.")
+        self.setup()
 
         with open("./lib/bot/token", "r", encoding="utf-8") as tf:
             self.TOKEN = tf.read()
@@ -53,8 +81,7 @@ class Bot(BotBase):
         if err == "on_command_error":
             await args[0].send("Something went wrong.")
 
-        channel = self.get_channel(752482031228682340)
-        await channel.send("An error has occured. Please check the console.")
+        await self.stdout.send("An error has occured. Please check the console.")
         raise
 
     async def on_command_error(self, ctx, exc):
@@ -77,13 +104,10 @@ class Bot(BotBase):
 
         if not self.ready:
             # Set connected state
-            self.ready = True
             self.guild = self.get_guild(435119526200213514)
             self.scheduler.start()
+            self.stdout = self.get_channel(752482031228682340)
 
-            # Set Command-dump channel
-            channel = self.get_channel(752482031228682340)
-            await channel.send("Now online!")
 
 #            embed = Embed(title="Now Online!",
 #                          description="KnockoutBot is now online.",
@@ -101,6 +125,11 @@ class Bot(BotBase):
 #
 #            await channel.send(file=File(fileLocationAvatar))
 
+            while not self.cogs_ready.all_ready():
+                await sleep(0.5)
+
+            await self.stdout.send("Now online!")
+            self.ready = True
             print("Bot Ready For Operation")
 
         else:

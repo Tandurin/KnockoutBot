890c24fcd4ed89031a8c541420e9d161adc25edd
Tandurin <dennisvlaanderen@gmail.com>
2020-09-22 15:37:08 +0200

    Basic knockout staged. ITerable error

--- lib/cogs/reactions.py
+++ lib/cogs/reactions.py
@@ -1,4 +1,8 @@
 from discord.ext.commands import Cog
+from discord.ext import commands
+from discord import Member
+from discord.utils import get
+from discord import Guild
 
 
 class Reactions(Cog):
@@ -10,14 +14,32 @@ class Reactions(Cog):
         if not self.bot.ready:
             self.bot.cogs_ready.ready_up("reactions")
 
+
     @Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        print(f"[RAW] {payload.member.display_name} reacted with {payload.emoji}")
+        print(f"[RAW] {payload.member.display_name} reacted with {payload.emoji.name}")
+        if payload.emoji.name == "KO_":
+            #Do code
+
+            msg = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+            member = msg.author
+            role_name = "Knockout"
+            members = Guild.members
+            for m in members:
+                try:
+                    await member.remove_roles(role_name)
+                except:
+                    print(f"Couldn't remove roles from {m}")        
+            print(f"Somebody needs to be knocked out!")
+
+            await payload.member.add_roles("Knockout")
+        else:
+            pass
 
     @Cog.listener()
     async def on_raw_reaction_remove(self, payload):
         member = self.bot.guild.get_member(payload.user_id)
-        print(f"[RAW] {member.display_name} removed the reaction of {payload.emoji}")
+        pass
 
 def setup(bot):
     bot.add_cog(Reactions(bot))
